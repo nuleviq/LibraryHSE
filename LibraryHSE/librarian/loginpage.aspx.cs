801608b80b580cc9c7af5f12e10455053a7935e2
Андрей Вершков <asvershkov@edu.hse.ru>
2019-12-17 22:24:29 +0300

    adding empty page_load and b1_click methods

--- LibraryHSE/librarian/loginpage.aspx.cs
+++ LibraryHSE/librarian/loginpage.aspx.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace LibraryHSE.librarian
 {
     public partial class loginpage : System.Web.UI.Page
     {
+        SqlConnection con = new SqlConnection(@"");
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void b1_Click(object sender, EventArgs e)
         {
 
         }
