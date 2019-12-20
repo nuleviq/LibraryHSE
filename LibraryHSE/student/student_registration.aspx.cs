97dc383d845a649c24378905b19f8b97a1c09517
Андрей Вершков <asvershkov@edu.hse.ru>
2019-12-20 20:27:44 +0300

    backend for student registration

--- LibraryHSE/student/student_registration.aspx.cs
+++ LibraryHSE/student/student_registration.aspx.cs
@@ -11,9 +11,33 @@ namespace LibraryHSE.student1
 {
     public partial class student_registration : System.Web.UI.Page
     {
-        SqlConnection con = new SqlConnection(@""); 
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\lms.mdf;Integrated Security=True"); 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (con.State==ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open()
+        }
+
+        protected void b1_Click(object sender, EventArgs e)
+        {
+            string randomno = RandomPassword.GetRandomPassword(10) + ".jpg";
+            string path = "";
+            f1.SaveAs(Request.PhysicalApplicationPath + "/student/student_img/" + randomno.ToString());
+            path = "student/student_img." + randomno.ToString();
+           
+            
+
+
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "";
+            cmd.ExecuteNonQuery();
+
+            Response.Write("<script>alert('record inserted successfully');</script>");
+
 
         }
     }
