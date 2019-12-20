51000814f24a8589c10773ce9c17bbea5f8eb74f
Андрей Вершков <asvershkov@edu.hse.ru>
2019-12-20 21:40:34 +0300

    backend for student login

--- LibraryHSE/student/student_login.aspx.cs
+++ LibraryHSE/student/student_login.aspx.cs
@@ -4,14 +4,44 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace LibraryHSE.student
 {
     public partial class student_login : System.Web.UI.Page
     {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\killmepls\source\repos\libraryhse\LibraryHSE\App_Data\lms.mdf;Integrated Security=True");
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
 
+        }
+        protected void b1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from student_registration where username='" + username.Text + "' and password='" + password.Text + "' and approved='yes'";
+            cmd.ExecuteNonQuery();
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            sqlDataAdapter.Fill(dataTable);
+            i = Convert.ToInt32(dataTable.Rows.Count.ToString());
+            if (i > 0)
+            {
+                Response.Redirect("test.aspx");
+            }
+            else
+            {
+                error.Style.Add("display", "block");
+            }
         }
     }
 }
